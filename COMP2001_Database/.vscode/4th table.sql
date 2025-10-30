8f103a7f189764c5d75958e934bb7002b803f087
Alfeido <jenkinsalfie08@gmail.com>
2025-10-30 15:16:15 +0000

    Finished database

---
+++ COMP2001_Database/.vscode/4th table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE CW1.OnlineDetails (
+    UID INT PRIMARY KEY,
+    Pref_Activities VARCHAR(256),
+    Lang VARCHAR(20),
+    About_Me VARCHAR(256),
+    Units VARCHAR(10)
+);
+
+SELECT * FROM CW1.LoginAccessDetails;
